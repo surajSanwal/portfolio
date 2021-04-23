049332a2f0a5811e973652609faa189f66c3dee8
surajSanwal <sonusanwal65@gmail.com>
2021-04-23 09:27:16 +0530

    project fix

---
+++ pages/data/index.ts
@@ -0,0 +1,150 @@
+export const services = [
+  {
+    src: "../typescript.png",
+    srcset: "../typescript.png",
+    title: "TypeScript",
+    link: "https://www.typescriptlang.org/",
+  },
+  {
+    src: "../javascript.png",
+    srcset: "../javascript.png",
+    title: "JavaScript",
+    link: "https://www.javascript.com/",
+  },
+  {
+    src: "../react.png",
+    srcset: "../react.png",
+    title: "ReactJs",
+    link: "https://reactjs.org/",
+  },
+  {
+    src: "../react.png",
+    srcset: "../react.png",
+    title: "React Native",
+    link: "https://reactnative.dev/",
+  },
+  {
+    src: "../angular.png",
+    srcset: "../angular.png",
+    title: "Angular",
+    link: "https://angular.io/",
+  },
+  {
+    src: "../docker.png",
+    srcset: "../docker.png",
+    title: "Docker",
+    link: "https://www.docker.com/",
+  },
+  {
+    src: "../graphql.png",
+    srcset: "../graphql.png",
+    title: "GraphQl",
+    link: "https://graphql.org/",
+  },
+  {
+    src: "../node.png",
+    srcset: "../node.png",
+    title: "Node",
+    link: "https://nodejs.org/en/",
+  },
+  {
+    src: "../nextjs-lg.png",
+    srcset: "../nextjs-lg.png",
+    title: "NextJs",
+    link: "https://nextjs.org/",
+  },
+  // {
+  //   src: "/frontity.svg",
+  //   srcset: "/frontity.svg",
+  //   title: "Frontity",
+  //   link: "https://frontity.org/",
+  // },
+];
+
+export const projects = [
+  {
+    src: "../liviausa.png",
+    title: "Livia USA",
+    link: "https://liviausa.com/",
+  },
+  {
+    src: "../liviaConnect.png",
+    title: "Livia Connect",
+    link: "https://liviaconnect.com/",
+  },
+  {
+    src: "../liviaRx.png",
+    title: "Livia Rx",
+    link: "https://liviarx.com/",
+  },
+  {
+    src: "../adidas.png",
+    title: "Adidas iOS",
+    link: "https://apps.apple.com/us/app/adidas/id1266591536",
+  },
+  {
+    src: "../adidas.png",
+    title: "Adidas Android",
+    link: "https://play.google.com/store/apps/details?id=com.adidas.app",
+  },
+  {
+    src: "../cidr.png",
+    title: "Circular Drive(CIDR) iOS",
+    link: "https://apps.apple.com/us/app/circulardrive/id1438519496?ls=1",
+  },
+  {
+    src: "../cidr.png",
+    title: "Circular Drive(CIDR) Android",
+    link: "https://play.google.com/store/apps/details?id=com.cidr",
+  },
+  {
+    src: "../act.png",
+    title: "ACT Nurses Portal",
+    link: "http://acthomehealthservices.com/nurse-portal/",
+  },
+  {
+    src: "../ajivar.png",
+    title: "Ajivar (iOS)-The Resiliency App",
+    link: "https://apps.apple.com/us/app/u-ajivar-resiliency-app/id1469213798",
+  },
+  {
+    src: "../ajivar.png",
+    title: "Ajivar (Android)-The Resiliency App",
+    link: "https://play.google.com/store/apps/details?id=com.ajivar.utajivar&hl=en",
+  },
+  {
+    src: "../ohc.png",
+    title: "Overture Health Care (iOS)",
+    link: "https://apps.apple.com/in/app/overture-health-care/id1469870955",
+  },
+  {
+    src: "../ohc.png",
+    title: "Overture Health Care (Android)",
+    link: "https://play.google.com/store/apps/details?id=com.overture.health&hl=en",
+  },
+  {
+    src: "../qvik.png",
+    title: "Qvik Task",
+    link: "http://54.71.18.74:4601/",
+  },
+  {
+    src: "../procure.png",
+    title: "Procure Network Core",
+    link: "https://core.procurenetworks.com/",
+  },
+  {
+    src: "../procure.png",
+    title: "Procure Network Order",
+    link: "https://order.procurenetworks.com/",
+  },
+  {
+    src: "../project-reachlive.png",
+    title: "ReachLive",
+    link: "http://reachlive.com/",
+  },
+  {
+    src: "/project-reachlive.png",
+    title: "ReachLive Admin",
+    link: "http://admin.reachlive.com/",
+  },
+];
